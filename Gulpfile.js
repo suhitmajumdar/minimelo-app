cb2619a9d819fc048c38acef96d22ce3de289dcf
Vincent Prigent <vincent.prigent@etu.u-bordeaux.fr>
2016-07-16 15:37:56 +0200

    Migrate from scss to sass

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,11 +2,11 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('styles', function() {
-    gulp.src('www/sass/*.scss')
+    gulp.src('www/sass/*.sass')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('www/css/'));
 });
 
 gulp.task('default',function() {
-    gulp.watch('www/sass/*.scss',['styles']);
+    gulp.watch('www/sass/*.sass',['styles']);
 });
